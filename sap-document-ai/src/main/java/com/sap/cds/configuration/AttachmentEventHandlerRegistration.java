84034c6baa86fcc10287dfd381d5ba3fe0d42dd9
samyuktaprabhu <33195453+samyuktaprabhu@users.noreply.github.com>
2026-05-27 09:36:17 +0200

    review fixes

--- sap-document-ai/src/main/java/com/sap/cds/configuration/AttachmentEventHandlerRegistration.java
+++ sap-document-ai/src/main/java/com/sap/cds/configuration/AttachmentEventHandlerRegistration.java
@@ -4,7 +4,7 @@ import com.sap.cds.handlers.AttachmentEventHandler;
 import com.sap.cds.services.runtime.CdsRuntimeConfiguration;
 import com.sap.cds.services.runtime.CdsRuntimeConfigurer;
 
-public class Registration implements CdsRuntimeConfiguration {
+public class AttachmentEventHandlerRegistration implements CdsRuntimeConfiguration {
     @Override
     public void eventHandlers(CdsRuntimeConfigurer configurer) {
         configurer.eventHandler(new AttachmentEventHandler());
