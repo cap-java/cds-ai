8a3e5f3628e18bf7f6daae30d4c077c69b222467
samyuktaprabhu <33195453+samyuktaprabhu@users.noreply.github.com>
2026-05-27 10:51:47 +0200

    revert unintended change

--- sap-document-ai/src/main/java/com/sap/cds/configuration/AttachmentEventHandlerRegistration.java
+++ sap-document-ai/src/main/java/com/sap/cds/configuration/AttachmentEventHandlerRegistration.java
@@ -1,12 +1,20 @@
 package com.sap.cds.configuration;
 
 import com.sap.cds.handlers.AttachmentEventHandler;
+import com.sap.cds.orchestrator.ExtractionOrchestrator;
+import com.sap.cds.orchestrator.ExtractionService;
+import com.sap.cds.services.persistence.PersistenceService;
 import com.sap.cds.services.runtime.CdsRuntimeConfiguration;
 import com.sap.cds.services.runtime.CdsRuntimeConfigurer;
 
 public class AttachmentEventHandlerRegistration implements CdsRuntimeConfiguration {
     @Override
     public void eventHandlers(CdsRuntimeConfigurer configurer) {
-        configurer.eventHandler(new AttachmentEventHandler());
+        PersistenceService persistenceService = configurer.getCdsRuntime()
+                .getServiceCatalog()
+                .getService(PersistenceService.class, PersistenceService.DEFAULT_NAME);
+
+        ExtractionService extractionService = new ExtractionOrchestrator(persistenceService);
+        configurer.eventHandler(new AttachmentEventHandler(extractionService));
     }
 }
