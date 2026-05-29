8ddf50c821361191abd1032a98a0bf88d3f11a64
samyuktaprabhu <33195453+samyuktaprabhu@users.noreply.github.com>
2026-05-29 17:51:40 +0200

    tiny improvements / comments

--- sap-document-ai/src/main/java/com/sap/cds/configuration/AttachmentEventHandlerRegistration.java
+++ sap-document-ai/src/main/java/com/sap/cds/configuration/AttachmentEventHandlerRegistration.java
@@ -7,16 +7,33 @@ import com.sap.cds.service.ExtractionService;
 import com.sap.cds.service.ExtractionServiceImpl;
 import com.sap.cds.services.ServiceCatalog;
 import com.sap.cds.services.persistence.PersistenceService;
+import com.sap.cds.services.runtime.CdsRuntime;
 import com.sap.cds.services.runtime.CdsRuntimeConfiguration;
 import com.sap.cds.services.runtime.CdsRuntimeConfigurer;
 
 public class AttachmentEventHandlerRegistration implements CdsRuntimeConfiguration {
     @Override
     public void eventHandlers(CdsRuntimeConfigurer configurer) {
-        ServiceCatalog serviceCatalog = configurer.getCdsRuntime().getServiceCatalog();
-        PersistenceService persistenceService = serviceCatalog.getService(PersistenceService.class, PersistenceService.DEFAULT_NAME);
-        DocumentAiProcessingService documentAiProcessingService = new DefaultDocumentAiProcessingService();
-        ExtractionService extractionService = new ExtractionServiceImpl(persistenceService, documentAiProcessingService);
-        configurer.eventHandler(new AttachmentEventHandler(extractionService));
+        CdsRuntime runtime = configurer.getCdsRuntime();
+        ServiceCatalog serviceCatalog = runtime.getServiceCatalog();
+
+        // framework-managed dependency
+        PersistenceService persistenceService =
+                serviceCatalog.getService(
+                        PersistenceService.class,
+                        PersistenceService.DEFAULT_NAME);
+
+        // internal
+        DocumentAiProcessingService documentAiProcessingService =
+                new DefaultDocumentAiProcessingService();
+
+        ExtractionService extractionService =
+                new ExtractionServiceImpl(
+                        persistenceService,
+                        documentAiProcessingService);
+
+        // register event handler with CAP runtime
+        configurer.eventHandler(
+                new AttachmentEventHandler(extractionService));
     }
 }
