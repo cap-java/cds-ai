c2b35eda0e48739d6960b22d1ffcdeef1ba00e34
Lisa Julia Nebel <lisa.nebel@sap.com>
2026-06-15 17:19:09 +0200

    Add comment on why we the @FunctionalInterface annotation is useful

--- cds-feature-recommendations/src/main/java/com/sap/cds/feature/recommendation/api/RecommendationClientResolver.java
+++ cds-feature-recommendations/src/main/java/com/sap/cds/feature/recommendation/api/RecommendationClientResolver.java
@@ -5,6 +5,8 @@ package com.sap.cds.feature.recommendation.api;
 
 import com.sap.cds.feature.aicore.api.AICoreService;
 
+// The annotation @FunctionalInterface ensures this interface has only one method, such that
+// callers can supply a custom client by providing this one method e.g. via a lambda.
 @FunctionalInterface
 public interface RecommendationClientResolver {
 
